498b3e9ff689d033164069b67ea13b4ebaad83c0
rrosenb12 <rebecca.rosenberg@datadoghq.com>
2021-10-14 12:11:31 -0400

    variables.py

---
+++ 00-Python Object and Data Structure Basics/variables.py
@@ -0,0 +1,15 @@
+a = 5
+print('a is', a)
+# 5
+a = 10
+print('a is', a)
+#10
+a = a + a
+print('a is', a)
+#20
+print('a + a is', a + a)
+#40
+print('a is still', a)
+#20
+print("a's type is", type(a))
+#integer
