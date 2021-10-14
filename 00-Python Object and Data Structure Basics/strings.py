c7426f52205929e2acd692afcd508ea3351e3f1d
rrosenb12 <rebecca.rosenberg@datadoghq.com>
2021-10-14 12:20:46 -0400

    strings.py

---
+++ 00-Python Object and Data Structure Basics/strings.py
@@ -0,0 +1,13 @@
+# Character    : h  e  l  l  o
+# Index        : 0  1  2  3  4
+# Reverse Index: 0 -4 -3 -2 -1
+
+# Slicing (grabbing a subsection of multiple characters)
+## [start:stop:step]
+### start: index where the slicing will start
+### stop: index where the slicing will go up to (AKA, slice does not include this index)
+### step: size of "jump" of slice
+
+print(len('hello'))
+# 5
+# len() returns length of string
