84addbdc9a0a7b3be6f92e5620a09c7b7f4da85b
rrosenb12 <rebecca.rosenberg@datadoghq.com>
2022-04-28 21:31:09 -0400

    show board

--- 04-Milestone Project - 1/tic-tac-toe.py
+++ 04-Milestone Project - 1/tic-tac-toe.py
@@ -1,3 +1,5 @@
+board = ['1','2','3','4','5','6','7','8','9']
+
 def pick_marker():
     player_1 = input('Player 1, do you want X or O: ').upper()
     if player_1 not in ['X', 'O']:
@@ -10,7 +12,13 @@ def pick_marker():
         else:
             player_2 = 'X'
             print("PLAYER 1:", player_1, "PLAYER 2:", player_2)
+        show_board(board)
 
-
+def show_board(board):
+    print(board[0] + '|' +board[1]+'|'+board[2])
+    print('-|-|-')
+    print(board[3] + '|' +board[4]+'|'+board[5])
+    print('-|-|-')
+    print(board[6] + '|' +board[7]+'|'+board[8])
 
 pick_marker()
