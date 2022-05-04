e701e8bf9992b9a3bc56d0f476e5f606fc3006aa
rrosenb12 <rebecca.rosenberg@datadoghq.com>
2022-05-03 22:49:17 -0400

    problem 2 oop homework

--- 05-Object Oriented Programming/oop-homework.py
+++ 05-Object Oriented Programming/oop-homework.py
@@ -1,8 +1,7 @@
-#Problem 1
+# Problem 1
 ## Make a Line class method that accepts coordinates as a pair of tuples and returns the slope and distance of the line
 
 from cmath import sqrt
-
 
 class Line:
     def __init__(self, coor1, coor2):
@@ -20,4 +19,23 @@ coordinate2 = (8,10)
 
 li = Line(coordinate1, coordinate2)
 
-print(li.slope())
+# Problem 2
+## Create a Cylinder class that calculates the volume and surface area
+
+class Cylinder:
+
+    pi = 3.14
+
+    def __init__(self,height=1,radius=1):
+        self.height = height
+        self.radius = radius 
+    
+    def volume(self):
+        return Cylinder.pi * self.radius**2 * self.height 
+
+    def surface_area(self):
+        return((2 * Cylinder.pi * self.radius * self.height) + (2 * Cylinder.pi * self.radius**2))
+
+c = Cylinder(2,3)
+
+print(c.surface_area())
