25445ac2f37454bf17572bc3641904710a9cbe34
rrosenb12 <rebecca.rosenberg@datadoghq.com>
2022-05-10 15:26:51 -0400

    errors and exceptions hw

---
+++ 07-Errors and Exception Handling/errors-and-exceptions-hw.py
@@ -0,0 +1,43 @@
+### Problem 1
+## Handle the exception thrown by the code below by using try and except blocks.
+
+from tokenize import String
+from typing import Type
+
+
+for i in ['a','b','c']:
+    try:
+        print(i**2)
+    except TypeError:
+        print("You can't square a letter")
+        break
+
+### Problem 2
+## Handle the exception thrown by the code below by using try and except blocks. Then use a finally block to print 'All Done.'
+
+x = 5
+y = 0
+
+try:    
+    z = x/y
+except ZeroDivisionError:
+    print("You can't divide by 0")
+finally:
+    print("All Done")
+
+### Problem 3
+## Write a function that asks for an integer and prints the square of it. Use a while loop with a try, except, else block to account for incorrect inputs.
+
+def ask():
+    user_input = ''
+    while type(user_input) == str:
+        try:
+            val = int(input("Please enter an integer: "))
+        except: 
+            print("That is not an integer")
+            continue 
+        else:
+            user_input = val
+            print("All done")
+
+ask()
