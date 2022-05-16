d5a84b9c44a4f184d331e8547df8ff48598e1c30
rrosenb12 <rebecca.rosenberg@datadoghq.com>
2022-05-16 16:01:14 -0400

    milestone project 2

---
+++ 08-Milestone Project - 2/milestone-project-2.py
@@ -0,0 +1,96 @@
+# OOP BlackJack #
+
+# Computetr dealer, human player
+# Start with normal deck of cards
+# Create representation of a deck
+
+# Human player has a bank roll
+## Attribute for bank roll (like a bank account)
+## From bank roll, human player will place a bet depending on whether or not they will win the set of hands
+
+# Player starts with two cards face up
+# Dealer starts with 1 card face up and 1 card face down
+# Player goes first
+# Player goal is to get closer to total value of 21 before dealer does
+# Player can hit, to receive another card, or stay, to not receive any cards
+# After player turn, if the player is under 21, dealer hits until they beat the player or bust
+# If player keeps hitting over 21 before dealer goes, they bust and lose the bet
+# Game is over and dealer collects money
+
+# Face cards count as value of 10
+# Aces can count as 1 or 11, whichver is preferable to player
+
+import random
+
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':[1,11]}
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+class Card:
+    # Each card has a suit, rank, and value
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+class Deck:
+    # Instantiate new deck with all 52 card objects -> hold as a list of card objects
+    ## This means the deck class will return card class object instances
+    # Shuffle a Deck through method call
+    # Deal cards from the deck object
+    def __init__(self):
+        self.deck = []
+
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit,rank)
+                self.deck.append(created_card)
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        return self.deck.pop()
+
+    def __str__(self):
+        return f'I have {len(self.deck)} cards'
+
+new_deck = Deck()
+new_deck.shuffle()
+"""
+print(new_deck)
+- returning "I have 52 cards"
+print(new_deck.decj[0].value)
+- returning [1,11] if Ace
+print(len(new_deck.deck))
+- returning 52
+"""
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+    
+    def adjust_for_ace(self):
+        pass
+
+    def __str__(self):
+        return f'{self.cards}'
+
+my_hand = Hand()
+my_hand.add_card(new_deck.deal())
+my_hand.add_card(new_deck.deal())
+for card in my_hand.cards:
+    print(card)
+
+dealer_hand = Hand()
+dealer_hand.add_card(new_deck.deal())
+dealer_hand.add_card(new_deck.deal())
+print(dealer_hand.cards[0])
