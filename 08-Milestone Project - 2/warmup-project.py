821ce7e64ed37e39b2d98968a9ca5e12f8379c06
rrosenb12 <rebecca.rosenberg@datadoghq.com>
2022-05-16 13:26:17 -0400

    player class

--- 08-Milestone Project - 2/warmup-project.py
+++ 08-Milestone Project - 2/warmup-project.py
@@ -1,4 +1,5 @@
 # from unittest.util import three_way_cmp
+from hashlib import new
 import random
 
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
@@ -61,3 +62,42 @@ my_card = new_deck.deal_one()
 
 # Will be 51 because we dealt 1
 ## print(len(new_deck.all_cards))
+
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        return self.all_cards.pop(0) 
+
+    def add_cards(self,new_cards):
+        # For multiple card objects -> extend adds list to list without nesting it. 
+        ## For example:
+        ### list = [a,b,c]
+        ### new_list = [d,e,f]
+        ### list.extend(new_list) = [a,b,c,d,e,f]
+        ### vs
+        ### list.append(new_list) = [a,b,c,[d,e,f]]
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        # For a single card object
+        else:
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+new_player = Player("Jose")
+print(new_player)
+# 0 cards
+new_player.add_cards(my_card)
+print(new_player)
+# 1 card
+print(new_player.all_cards[0])
+new_player.add_cards([my_card, my_card, my_card])
+print(new_player)
+# 4 cards
+new_player.remove_one()
+print(new_player)
+# 3 cards
